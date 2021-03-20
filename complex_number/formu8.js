9e1d06f2b0cb0c34d22745fa7bb6e92fc1b3227b
Lucxs95 <78850304+Lucxs95@users.noreply.github.com>
2021-03-20 18:47:21 +0100

    formu8 add

---
+++ complex_number/formu8.js
@@ -0,0 +1,21 @@
+var myQuestions = [
+	{
+		question: "Si on a -a-jb ou se trouve le point par rapport a l'axe ? ",
+		answers: {
+			a: 'En haut à gauche ',
+			b: 'En bas à gauche',
+			c: 'En bas à droite'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Si on a 10-3j ou se trouve le point par rapport a l'axe ?'",
+		answers: {
+			a: '10 unités a droite 3 unités en haut ',
+			b: '10 unités a droite 3 unités en bas ',
+			c: '10 unités a gauche 3 unités en haut ',
+			d: '10 unités a gauche 3 unités en bas'
+		},
+		correctAnswer: 'b'
+	}
+];
