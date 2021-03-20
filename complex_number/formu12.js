79923560edd3ffd67c949cb5737193f7915d454e
Lucxs95 <78850304+Lucxs95@users.noreply.github.com>
2021-03-20 14:27:03 +0100

    add formulaire nb complexe

---
+++ complex_number/formu12.js
@@ -0,0 +1,20 @@
+var myQuestions = [
+	{
+		question: "Dans quel ensemble sont définis les nombres complexes ?",
+		answers: {
+			a: 'Z',
+			b: 'R',
+			c: 'C'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "j&sup2; = ?",
+		answers: {
+			a: '-1',
+			b: 'j',
+			c: '1'
+		},
+		correctAnswer: 'a'
+	}
+];
