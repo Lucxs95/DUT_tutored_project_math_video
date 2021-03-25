c6d1209e302233967eca79c6ab720433acdfc91f
Lucxs95 <78850304+Lucxs95@users.noreply.github.com>
2021-03-25 12:52:26 +0100

    add formu

---
+++ log/formu2.js
@@ -0,0 +1,20 @@
+var myQuestions = [
+	{
+		question: "Valeurs de base a connaitre : exp(0)=",
+		answers: {
+			a: '1',
+			b: '0',
+			c: '-1'
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "D'après la réciproque on a e<sup>ln(1)</sup>:",
+		answers: {
+			a: '0',
+			b: '1;',
+			c: 'e<sup>1</sup>'
+		},
+		correctAnswer: 'b'
+	}
+];
